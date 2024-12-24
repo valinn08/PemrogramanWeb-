7c6faab711c90f5d879b191753881aa3b50f8e4c
valinn08 <email@domain.com>
2024-12-24 09:13:06 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+document.write("Helo World");
+document.getElementById("demo").innerHTML = "Ini adalah teks dari JavaScript";
+window.alert("Ini adalah teks dari JavaScript alert");
+
+console.log("Ini hasil Console Log");
+console.info("Ini hasil Console Infor");
+console.warn("Ini hasil Console Warning");
+console.error("Ini hasil Console Error");
+
+const dataTable = [
+  { id: 1, nama: "andre" },
+  { id: 2, nama: "Joko" },
+];
+
+console.table(dataTable);
